9d1581232c922a9816ed9534a05002662ff08fcf
Trish Griego <pmgriego@gmail.com>
2016-10-31 16:01:35 -0700

    Created homepages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'homepages/index'
+
+  get 'homepages/create'
+
+  get 'homepages/new'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
