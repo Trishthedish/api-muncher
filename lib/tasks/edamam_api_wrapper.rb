1be05ca851179c94c87d9cafc7a29ef7cdace3be
Trish Griego <pmgriego@gmail.com>
2016-10-31 17:19:05 -0700

    added a edamam_wrapper to my lib

--- lib/tasks/edamam_api_wrapper.rb
+++ lib/tasks/edamam_api_wrapper.rb
@@ -2,6 +2,11 @@ require 'httparty'
 require 'awesome_print'
 
 class Edamam_Api_Wrapper
-  BASE_URL = ""
+  # # The BASE_URL is the beginning of every request.
+  BASE_URL = "https://api.edamam.com/search"
+  app_id = ENV["app_id"]
+  app_key = ENV["app_key"]
+
+
 
 end
