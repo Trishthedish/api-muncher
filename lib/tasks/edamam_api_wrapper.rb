57a244172d54be78fdc7d08a4290df6232411ac8
Trish Griego <pmgriego@gmail.com>
2016-10-31 17:34:23 -0700

    Adjusted api wrapper

--- lib/tasks/edamam_api_wrapper.rb
+++ lib/tasks/edamam_api_wrapper.rb
@@ -7,10 +7,11 @@ class Edamam_Api_Wrapper
   app_id = ENV["app_id"]
   app_key = ENV["app_key"]
 
-  def self.search_recipes()
+  def self.search_recipes
     url = BASE_URL + "search" + "?q=chicken&app_id=#{app_id}&app_key=#{app_key}"
+
     data = HTTParty.get(url)
-    
+
   end
 
 
