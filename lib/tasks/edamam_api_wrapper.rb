4cb050f369b3f59f84f3f949e3cc738af44a9f8f
Trish Griego <pmgriego@gmail.com>
2016-10-31 17:29:51 -0700

    created api wrapper

--- lib/tasks/edamam_api_wrapper.rb
+++ lib/tasks/edamam_api_wrapper.rb
@@ -3,10 +3,15 @@ require 'awesome_print'
 
 class Edamam_Api_Wrapper
   # # The BASE_URL is the beginning of every request.
-  BASE_URL = "https://api.edamam.com/search"
+  BASE_URL = "https://api.edamam.com/"
   app_id = ENV["app_id"]
   app_key = ENV["app_key"]
 
+  def self.search_recipes()
+    url = BASE_URL + "search" + "?q=chicken&app_id=#{app_id}&app_key=#{app_key}"
+    data = HTTParty.get(url)
+    
+  end
 
 
 end
