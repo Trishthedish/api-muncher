d2d630b66ceeee168234059568025634b06f9581
Trish Griego <pmgriego@gmail.com>
2016-11-04 13:27:11 -0700

    Leaving this branch behind. JS not working, Orbit not wokring, ALL is bleh

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,11 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
+
 //= require_tree .
+var ready;
+ready = function() {
+  $(document).foundation();
+};
+$(document).ready(ready);
+$(document).on('page:load', ready);
