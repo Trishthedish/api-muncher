2121c34fd0092bf1c18b231b29f467243bb02f1a
Trish Griego <pmgriego@gmail.com>
2016-11-04 14:12:39 -0700

    Added a few more tests and feel like more time should be spent on some css

--- test/lib/edamam_api_wrapper_test.rb
+++ test/lib/edamam_api_wrapper_test.rb
@@ -4,7 +4,7 @@ require 'recipe'
 
 class Edamam_Api_WrapperTest < ActionController::TestCase
 
-  test "can retrieve recipes from API" do
+  test "can retrieve recipes from API will return a non-empty array when reasonable term used." do
 
     VCR.use_cassette("search_results") do
       search_results = Edamam_Api_Wrapper.search_recipes("juice")
@@ -18,19 +18,37 @@ class Edamam_Api_WrapperTest < ActionController::TestCase
     end
   end
 
-  test "what happens if no input "
+# not fully flushed out logic.
+  # test "search returns the correct length of array(based on API call)" do
+  #   VCR.use_cassette("limit_100") do
+  #
+  #   end
+  # end
+
+  # test "what happens when you enter just spaces"
+
+    test "blank search, made to recipes, returns array with nothing in it" do
+      VCR.use_cassette('blank_search') do
+        search_results = Edamam_Api_Wrapper.search_recipes(nil)
+        assert_empty(search_results)
+      end
+    end
+    # Found test about tested this hypothetical test "test what happens when you enter just spaces"
 
 
 
-  test "what happens when muliple words our entered."
+
+   test "what happens when muliple words our entered." do
+     #http://localhost:3000/recipes/search?utf8=%E2%9C%93&query=apple+gin+smoke this search returns only 2 entries.
+     VCR.use_cassette("three_words") do
+      search_results = Edamam_Api_Wrapper.search_recipes("apple gin smoke")
+      assert_not_empty(search_results)
+      assert_equal(search_results.length, 2)
+     end
+   end
 
 
-  test "what happens when you enter just spaces.sx"
-
-
-  
-
-
+  #
   # test "Retrieves nill when the token is wrong" do
   #   VCR.use_cassette("") do
   #     search_results = Edamam_Api_Wrapper.search_recipes ""
@@ -38,7 +56,7 @@ class Edamam_Api_WrapperTest < ActionController::TestCase
   #   end
   # end
 
-# test isn't working either.
+# Not sure this is necessary. 
 
   # test "can send properly formattd msg" do
   #   VCR.use_cassette("get_recipe") do
@@ -49,12 +67,6 @@ class Edamam_Api_WrapperTest < ActionController::TestCase
   #     asset response["message"]["subtype"] == "bot_message"
   #   end
   # end
-
-
-
-
-
-
 
 
 
