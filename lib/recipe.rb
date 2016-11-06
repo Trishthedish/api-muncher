3076dd88366300b3bdea6005a05e46fb2e4b246f
Trish Griego <pmgriego@gmail.com>
2016-11-06 14:45:29 -0800

    Added dietary information on show page. No idea why, health labels aren't rendering properly.

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -1,16 +1,18 @@
 class Recipe
-  attr_reader :url, :link, :r_source, :name, :photo, :uri, :ingredientLines
+  attr_reader :url, :link, :r_source, :diet_labels, :health_labels, :name, :photo, :uri, :ingredientLines
 
-  def initialize(url, r_source, link, name, photo, uri, ingredientLines)
+  def initialize(url, r_source, diet_labels, link, health_labels, name, photo, uri, ingredientLines)
     @url = url
     @link = link
     @r_source = r_source
+    @diet_labels = diet_labels
+    @health_labels = health_labels
     @name = name
     @photo = photo
     @uri = uri
     @ingredientLines = ingredientLines
 
-    if url == nil || name == nil || photo == nil || uri = nil || ingredientLines == nil || url == "" || name == "" || photo == "" || uri = "" || ingredientLines == "" || link == nil || link == "" || r_source == nil || r_source == ""
+    if url == nil || name == nil || photo == nil || uri = nil || ingredientLines == nil || url == ""  || name == "" || photo == "" || uri = "" || ingredientLines == "" || link == nil || link == "" || r_source == nil || r_source == "" || diet_labels == nil || diet_labels == "" || health_labels == nil || health_labels == ""
 
     end
 
