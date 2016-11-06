4c903dd2a0e9d890112d05c0067baded059716fc
Trish Griego <pmgriego@gmail.com>
2016-11-06 13:58:48 -0800

    links to other urls open in a new window.

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -1,14 +1,15 @@
 class Recipe
-  attr_reader :url, :name, :photo, :uri, :ingredientLines
+  attr_reader :url, :link, :name, :photo, :uri, :ingredientLines
 
-  def initialize(url, name, photo, uri, ingredientLines)
+  def initialize(url, link, name, photo, uri, ingredientLines)
     @url = url
+    @link = link
     @name = name
     @photo = photo
     @uri = uri
     @ingredientLines = ingredientLines
 
-    if url == nil || name == nil || photo == nil || uri = nil || ingredientLines == nil || url == "" || name == "" || photo == "" || uri = "" || ingredientLines == ""
+    if url == nil || name == nil || photo == nil || uri = nil || ingredientLines == nil || url == "" || name == "" || photo == "" || uri = "" || ingredientLines == "" || link == nil || link == ""
 
     end
 
